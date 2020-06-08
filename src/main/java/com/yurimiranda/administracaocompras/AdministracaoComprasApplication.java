937c3b89d753ede36ce2f2e74ba41a81b5325f22
Yuri Miranda <yurimlima11@gmail.com>
2020-06-08 16:06:14 -0300

    Projeto criado

---
+++ src/main/java/com/yurimiranda/administracaocompras/AdministracaoComprasApplication.java
@@ -0,0 +1,13 @@
+package com.yurimiranda.administracaocompras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdministracaoComprasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdministracaoComprasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yurimiranda/administracaocompras/AdministracaoComprasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yurimiranda.administracaocompras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdministracaoComprasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
