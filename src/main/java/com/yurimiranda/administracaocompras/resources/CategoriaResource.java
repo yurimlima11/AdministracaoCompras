69b136739cd0dad46b8ba0b9a09ea26897955af9
Yuri Miranda <yurimlima11@gmail.com>
2020-06-16 22:56:27 -0300

    Criando repository e service para entidade Categoria. Testes com  REST usando GET e entidade Categoria

--- src/main/java/com/yurimiranda/administracaocompras/resources/CategoriaResource.java
+++ src/main/java/com/yurimiranda/administracaocompras/resources/CategoriaResource.java
@@ -1,28 +1,26 @@
 package com.yurimiranda.administracaocompras.resources;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.yurimiranda.administracaocompras.entities.Categoria;
+import com.yurimiranda.administracaocompras.services.CategoriaService;
 
 @RestController
-@RequestMapping(value = "/categorias")
+@RequestMapping(value = "/api/categorias")
 public class CategoriaResource {
+	
+	@Autowired
+	private CategoriaService categoriaService;
 
-	@RequestMapping(method = RequestMethod.GET)
-	public List<Categoria> listar() {
-		Categoria cat1 = new Categoria(1, "Informatica");
-		Categoria cat2 = new Categoria(2, "Escritorio");
-		
-		List<Categoria> list = new ArrayList<Categoria>();
-		list.add(cat1);
-		list.add(cat2);
-		
-		return list;
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<?> listar(@PathVariable Integer id) {
+		Categoria categoria = categoriaService.find(id);
+		return ResponseEntity.ok().body(categoria);
 	}
 	
 }
---
+++ src/main/java/com/yurimiranda/administracaocompras/services/CategoriaService.java
@@ -0,0 +1,21 @@
+package com.yurimiranda.administracaocompras.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.yurimiranda.administracaocompras.entities.Categoria;
+import com.yurimiranda.administracaocompras.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+	public Categoria find(Integer id) {
+		Optional<Categoria> categoria = categoriaRepository.findById(id);
+		return categoria.orElse(null);
+	}
+}
