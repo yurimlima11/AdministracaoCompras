a2df18610bdff9389155ab646f8d2a70c0a4fc9b
Yuri Miranda <yurimlima11@gmail.com>
2020-08-30 10:37:58 -0300

    Paginação com parametros opcionais na requisição

--- src/main/java/com/yurimiranda/administracaocompras/controller/CategoriaController.java
+++ src/main/java/com/yurimiranda/administracaocompras/controller/CategoriaController.java
@@ -1,15 +1,18 @@
 package com.yurimiranda.administracaocompras.controller;
 
 import java.net.URI;
+
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -58,4 +61,17 @@ public class CategoriaController {
 		List<CategoriaDTO> listaCategorias = categorias.stream().map(categoria -> new CategoriaDTO(categoria)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listaCategorias);
 	}
-}
+	
+	@RequestMapping(value = "/page", method = RequestMethod.GET)
+	public ResponseEntity<Page<CategoriaDTO>> findPage(
+			@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage,
+			@RequestParam(value = "orderBy", defaultValue = "nome") String orderBy,
+			@RequestParam(value = "direction", defaultValue = "ASC")  String direction){
+
+		Page<Categoria> lista = categoriaService.findPage(page, linesPerPage, orderBy, direction);
+		Page<CategoriaDTO> listaDTO = lista.map(categoria -> new CategoriaDTO(categoria));
+		return ResponseEntity.ok().body(listaDTO);
+	}
+		
+	}
--- src/main/java/com/yurimiranda/administracaocompras/services/CategoriaService.java
+++ src/main/java/com/yurimiranda/administracaocompras/services/CategoriaService.java
@@ -6,6 +6,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.yurimiranda.administracaocompras.entities.Categoria;
@@ -47,5 +50,12 @@ public class CategoriaService {
 	public List<Categoria> findAll(){
 		List<Categoria> categorias = categoriaRepository.findAll();
 		return categorias;
+	}
+	
+	public Page	<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy,
+			String direction){
+		
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return categoriaRepository.findAll(pageRequest);
 	}
 }
