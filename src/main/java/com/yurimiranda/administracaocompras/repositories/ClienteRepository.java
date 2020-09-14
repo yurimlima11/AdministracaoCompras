cd6303f0d03c41e6170c041186841d855d3cbab3
Yuri Miranda <yurimlima11@gmail.com>
2020-09-14 18:55:16 -0300

    Validação customizada: e-mail não repetido na inserção de Cliente

--- src/main/java/com/yurimiranda/administracaocompras/repositories/ClienteRepository.java
+++ src/main/java/com/yurimiranda/administracaocompras/repositories/ClienteRepository.java
@@ -2,10 +2,13 @@ package com.yurimiranda.administracaocompras.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.yurimiranda.administracaocompras.entities.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
 
+	@Transactional(readOnly = true)
+	Cliente findByEmail(String email);
 }
--- src/main/java/com/yurimiranda/administracaocompras/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/yurimiranda/administracaocompras/services/validation/ClienteInsertValidator.java
@@ -6,13 +6,19 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.yurimiranda.administracaocompras.controller.exception.FieldMessage;
 import com.yurimiranda.administracaocompras.dto.ClienteNewDTO;
+import com.yurimiranda.administracaocompras.entities.Cliente;
 import com.yurimiranda.administracaocompras.enums.TipoClienteE;
+import com.yurimiranda.administracaocompras.repositories.ClienteRepository;
 import com.yurimiranda.administracaocompras.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO>{
 
+	@Autowired
+	private ClienteRepository clienteRepository;
 
 	@Override
 	public boolean isValid(ClienteNewDTO clienteNewDTO, ConstraintValidatorContext context) {
@@ -25,6 +31,12 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		
 		if(clienteNewDTO.getTipoCliente().equals(TipoClienteE.PESSOA_JURIDICA.getCod()) && !BR.isValidCNPJ(clienteNewDTO.getCpfOuCnpj())) {
 			lista.add(new FieldMessage("cpfOuCnpj", "CNPJ Inválido"));
+		}
+		
+		Cliente cliente = clienteRepository.findByEmail(clienteNewDTO.getEmail());
+		
+		if(cliente != null) {
+			lista.add(new FieldMessage("email", "E-mail já está em uso"));
 		}
 		
 		for (FieldMessage fieldMessage : lista) {
